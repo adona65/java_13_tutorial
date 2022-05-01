8725eb25bb2abaa10f7ea39d46040c3851b686f0
DESKTOP\adona65 <adona65>
2022-05-01 14:08:04 +0200

    - Add examples and clarify some explanations about Text Blocks - Add information about DOM and SAX new methods

--- src/tutorial_000/languageNewFeatures/_001_TextBlocks.java
+++ src/tutorial_000/languageNewFeatures/_001_TextBlocks.java
@@ -13,9 +13,10 @@ public class _001_TextBlocks {
          		+ "\"Name\" : \"Toto\",\r\n" 
          		+ "\"Registration_number\" : \"32\"\r\n" 
          		+ "}";
-		 /*  
+		/*  
 		 * With Text Blocks, we just would have to normally type our string (without line breakers or char to escape), and surround 
-		 * it with """ (triple double-quote) :
+		 * it with """ (triple double-quote). Beware, he opening triple-double quotes must be followed by a line terminator :
+		 * 
 		 	// Commented in order to avoid compilation error with more recent versions of JDK.	
 		   	String textBlockJSON = """
 	         {
@@ -23,18 +24,35 @@ public class _001_TextBlocks {
 	            "Registration_number" : "32"
 	         }
          	""";
-		 * 
+         *
+         * We will note that string created with Text Blocks, and string created without it but including line breaker chars would be equals :
+         * 
+	         String textBlock = """
+				Hello
+				Mister""";
+				
+		     String str = "Hello\Mister";
+		     
+		     textBlock.equals(str); // Would return "true".
+		     textBlock == str; // Would return "true".
+		 *
+		 */
+		   
+		/* 
 		 * New methods were also added to String for working with Text Blocks :
-		 *   - stripIndent() : removes incidental white spaces from the start and end of the string.
-		 *   - translateEscapes() : translate the escape sequences as per the string syntax.
+		 *   - stripIndent() : used to remove the incidental white space characters from the beginning and end of every line in the text block.
+		 *   	It doesn't modify the indentation inside the content (doesn't replace "\t" tabulations).
+		 *   - translateEscapes() : translate the escape sequences as per the string syntax (as if in a string literal).
 		 *   - formatted() : similar to String format() method to support formatting in text block strings.
 		 *   
 		 * For example, we may write the previous code snippet like this :
+		 * 
 		 	String textBlockJSON = """
 	         {
 	            "name" : "%s",
 	            "Registration_number" : "%s"
 	         }
 	        """.formatted("Toto", "32");
+	     *
 		 */
 }
